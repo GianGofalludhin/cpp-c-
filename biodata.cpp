39927207a58157e3b8bf3fe2b0f984ed8661194d
GianGofalludhin <114574685+GianGofalludhin@users.noreply.github.com>
2022-11-30 16:46:05 +0700

    Add files via upload

---
+++ biodata.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Gian"<< endl;
+    cout <<"kelas: 10 rpl 1"<< endl;
+    cout <<"nilai: 80,65"<< endl;
+    
+}
+
---
+++ test 2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "aplikasi test kedua";<<end;
+    cout <<"masukan angka pertama";
+    int angka1;
+    cin >>angka1;
+    cout <<"masukan angaka kedua";
+    int angka2;
+    cin >>angka2;
+    cout <<"masukan angka ketiga";
+    int angka3;
+    cin >>angka3;
+    int hasil;
+    hasil=angka*angka*angka3;
+    cout <<"hasil pembagian="<<hasil;
+}
